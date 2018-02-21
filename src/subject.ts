fd2d052f26737ad47e8841d07191d52cfcd566d3
Davide Fucci <davfuc@gmail.com>
2018-02-21 23:08:46 +0100

    first commit

---
+++ src/subject.ts
@@ -0,0 +1,19 @@
+// tslint:disable-next-line:no-var-requires
+const electron = require('electron');
+const { ipcRenderer } = electron;
+class Subject {
+  private ID: string;
+  private answers: boolean[];
+
+  constructor(ID: string) {
+    this.ID = ID;
+    this.answers = [];
+  }
+}
+
+function createSubject(e: any): void {
+  const id = document.querySelector('#subjectID') as HTMLInputElement;
+  const s = new Subject(id.value);
+  console.log(s);
+  ipcRenderer.send('subject:create', s);
+}
