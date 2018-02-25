53de6bdccc2c2a6600176a6a537a56515f2fb2ac
Davide Fucci <davfuc@gmail.com>
2018-02-25 22:40:17 +0100

    add preventDefault

--- src/subject.ts
+++ src/subject.ts
@@ -12,8 +12,10 @@ class Subject {
 }
 
 function createSubject(e: any): void {
+  e.preventDefault();
   const id = document.querySelector('#subjectID') as HTMLInputElement;
   const s = new Subject(id.value);
+  console.log('the subject');
   console.log(s);
   ipcRenderer.send('subject:create', s);
 }
