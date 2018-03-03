8def4c67c1055657ec5ec8b2ee304c9aba300d18
Davide Fucci <davfuc@gmail.com>
2018-03-03 23:37:23 +0100

    check timers

--- src/questions.ts
+++ src/questions.ts
@@ -1,23 +1,13 @@
-// tslint:disable:no-var-requires
 const fs = require('fs');
 const path = require('path');
 const BrowserWindow = electron.remote.BrowserWindow;
-enum questionsType {
-  Review,
-  Prose
-}
-
 class Question {
   private correctAnswer: boolean;
   private answer: boolean;
   private questionImage: string;
-  private type: questionsType;
+  private type: string;
 
-  constructor(
-    questionImage: string,
-    type: questionsType,
-    correctAnswer: boolean
-  ) {
+  constructor(questionImage: string, type: string, correctAnswer: boolean) {
     this.questionImage = questionImage;
     this.correctAnswer = correctAnswer;
     this.type = type;
@@ -26,34 +16,65 @@ class Question {
     return this.questionImage;
   }
 }
+
+let timeHandle: number;
+let restartTimeout: number;
+let questions: Question[] = this.createQuestions();
 function startCarousel(): void {
-  const questions: Question[] = this.createQuestionsSequence();
-  const el = document.querySelector('img');
-  questions.forEach((obj: Question, index: number) => {
-    const timer = setTimeout(() => {
-      console.log('foobar');
-      el.setAttribute(
-        'src',
-        `./assets/questions/review/${obj.getQuestionImage()}`
+  function nextPicture(qs: Question[]) {
+    const picture = qs.shift();
+    if (picture) {
+      replaceImage(picture);
+      this.timeHandle = setTimeout(() => nextPicture(qs), 5000);
+    } else {
+      const el = document.querySelector('img');
+      el.setAttribute('src', './assets/questions/fixation.jpg');
+      const btnDiv = document.getElementById('buttons');
+      btnDiv.style.visibility = 'hidden';
+      this.restartTimeout = setTimeout(
+        restart,
+        1000 * Math.floor(Math.random() * 6) + 2
       );
-    }, index * 2000);
-  });
+    }
+  }
+  nextPicture(questions);
+}
+function restart() {
+  console.log('restarting');
+  clearTimeout(this.restartTimeout);
+  const btnDiv = document.getElementById('buttons');
+  btnDiv.style.visibility = 'visible';
+  this.questions = this.createQuestions();
+  startCarousel();
+}
+function nextQuestion() {
+  console.log('nextQuestion');
+  clearTimeout(this.timeHandle);
+  startCarousel();
 }
 
-function createQuestionsSequence(): Question[] {
-  console.log('create seq');
-  const questionFolder = './assets/questions/review';
-  const questions: Question[] = [];
-  fs
-    .readdirSync(questionFolder)
-    .sort(() => 0.5 - Math.random())
-    .slice(0, 10)
-    .forEach((file: any) => {
-      const q = new Question(file, questionsType.Review, true);
-      questions.push(q);
-      console.log(q);
-    });
-  return questions;
+function replaceImage(obj: Question) {
+  const el = document.querySelector('img');
+  el.setAttribute('src', `./assets/questions/review/${obj.getQuestionImage()}`);
+}
+
+function createQuestions(): Question[] {
+  function createQuestionOfType(type: string): Question[] {
+    const questionFolder = `./assets/questions/${type}`;
+    const questionArray: Question[] = [];
+    fs
+      .readdirSync(questionFolder)
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 10)
+      .forEach((file: any) => {
+        const q = new Question(file, type, true);
+        questionArray.push(q);
+      });
+    return questionArray;
+  }
+  const qReview = createQuestionOfType('review');
+  const qProse = createQuestionOfType('prose');
+  return qReview;
 }
 
 function createQuestionWindow(e: any): void {
